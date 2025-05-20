0a12420a2a8e4b4d1dfcb7bd22bfdeadda90cb33
Rosa <costurezati@gmail.com>
2025-05-20 20:07:21 -0300

    .

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include('logic.php');
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,8 +14,9 @@ include('logic.php');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./ressources/css/style.css">
 </head>
+
 <body>
-    <header class="header"  id="header">
+    <header class="header" id="header">
         <a href="#" class="logo"><img src="./ressources/img/logo.png" alt=""></a>
         <nav class="navbar">
             <a href="#cadastro">Cadastro</a>
@@ -23,53 +25,53 @@ include('logic.php');
         <div id="menu-btn" class="fas fa-bars"></div>
     </header>
 
-<section class="cadastro" id="cadastro">
-    <div class="row">
-        <form action="/laragon/www/Back-End/insertDadoCategoria.php" method="POST">
-            <h2>Cadastrar Categoria</h2>
-            <label for="categoria">Categoria</label>
-            <input class="box" type="text" name="nome_categoria" id="nome_categoria" placeholder="Categoria">
-            <input class="btn" type="submit" value="Enviar">
-        </form>
-        
+    <section class="cadastro" id="cadastro">
+        <div class="row">
+            <form action="/laragon/www/Back-End/insertDadoCategoria.php" method="POST">
+                <h2>Cadastrar Categoria</h2>
+                <label for="categoria">Categoria</label>
+                <input class="box" type="text" name="nome_categoria" id="nome_categoria" placeholder="Categoria">
+                <input class="btn" type="submit" value="Enviar">
+            </form>
 
-        <form  method="POST">   
-            <h2>Cadastrar Produto</h2>         
-            
-            <label for="nome">Nome</label>
-            <input class="box" type="text" name="nome" id="nome" placeholder="Nome" required>
 
-            <label for="quantidade">Quantidade</label>
-            <input class="box" type="number" name="quantidade" id="quantidade" placeholder="Quantidade" required>
+            <form method="POST">
+                <h2>Cadastrar Produto</h2>
 
-            <label for="unidade">Unidade</label>
-            <input class="box" type="number" name="unidade" id="unidade" placeholder="Unidade" required>
+                <label for="nome">Nome</label>
+                <input class="box" type="text" name="nome" id="nome" placeholder="Nome" required>
 
-            <label for="preco">Preço</label>
-            <input class="box" type="number" name="preco" id="preco" placeholder="Preço" step="0.010"  required maxlength="10" min="0" max="9999999999" required>
+                <label for="quantidade">Quantidade</label>
+                <input class="box" type="number" name="quantidade" id="quantidade" placeholder="Quantidade" required>
 
-            <select class="box" name="categoria_id" required>
-            <option class="box" value="<?= $categoria['id'] ?>">Categoria</option>
-            <?php foreach ($categorias as $categoria): ?>
-                <option value="<?= $categoria['id_categoria'] ?>"><?= $categoria['nome_categoria'] ?></option>
-            <?php endforeach; ?>
-            </select>
+                <label for="unidade">Unidade</label>
+                <input class="box" type="number" name="unidade" id="unidade" placeholder="Unidade" required>
 
-            <input class="btn" name="adicionar" type="submit" value="Enviar">
+                <label for="preco">Preço</label>
+                <input class="box" type="number" name="preco" id="preco" placeholder="Preço" step="0.010" required maxlength="10" min="0" max="9999999999" required>
 
-        </form>
+                <select class="box" name="categoria_id" required>
+                    <option class="box" value="<?= $categoria['id'] ?>">Categoria</option>
+                    <?php foreach ($categorias as $categoria): ?>
+                        <option value="<?= $categoria['id_categoria'] ?>"><?= $categoria['nome_categoria'] ?></option>
+                    <?php endforeach; ?>
+                </select>
 
-        <div class="image">
-            <img src="./ressources/img/banner.png" alt="">
+                <input class="btn" name="adicionar" type="submit" value="Enviar">
+
+            </form>
+
+            <div class="image">
+                <img src="./ressources/img/banner.png" alt="">
+            </div>
         </div>
-    </div>
 
-        
-</section>
-<section class="estoque" id="estoque">
+
+    </section>
+    <section class="estoque" id="estoque">
         <div class="row">
             <div class="box">
-                <form method="get">    
+                <form method="get">
                     <select name="filtro" class="box" id="filtragem" onchange="this.form.submit()">
                         <option class="box" value="">Escolha a filtragem: </option>
                         <option class="box" value="crescente">Crescente de nome (A-Z)</option>
@@ -81,7 +83,7 @@ include('logic.php');
                 </form>
 
             </div>
-        <div class="box">             
+            <div class="box">
                 <h2>Produtos Cadastrados</h2>
                 <table>
                     <tr>
@@ -92,33 +94,34 @@ include('logic.php');
                         <th>Categoria</th>
                         <th>Preço</th>
                         <th>Ações</th>
-                        
+
                     </tr>
                     <?php foreach ($produtos as $product): ?>
                         <tr>
-                            <td><?=$product['id'];?></td>
-                            <td><?=($product['quantidade'] <= 5 ? '⚠️🚨' : '') . htmlspecialchars($product['nome']);?></td>
-                            <td><?=$product['quantidade'];?></td>
-                            <td><?=$product['unidade'];?></td>
-                            <td><?=$product['categoria'];?></td>
+                            <td><?= $product['id']; ?></td>
+                            <td><?= ($product['quantidade'] <= 5 ? '⚠️🚨' : '') . htmlspecialchars($product['nome']); ?></td>
+                            <td><?= $product['quantidade']; ?></td>
+                            <td><?= $product['unidade']; ?></td>
+                            <td><?= $product['categoria']; ?></td>
                             <td>R$<?= number_format($product['preco'], 2, ',', '.') ?></td>
-                            <td><a class="btn" href="./update.php">Editar</a><a class="delete-btn" href="?delete=<?=$product['id']; ?>" onclick="return confirm ('Tem certeza que deseja excluir?')">Excluir</a></td>  
+                            <td><a class="btn" href="./update.php">Editar</a><a class="delete-btn" href="?delete=<?= $product['id']; ?>" onclick="return confirm ('Tem certeza que deseja excluir?')">Excluir</a></td>
                         </tr>
-                        <?php endforeach; ?>
-                </table>  
+                    <?php endforeach; ?>
+                </table>
 
             </div>
         </div>
 
-        
-            
+
+
 
         </div>
-    
-        
-</section>
-<footer class="footer" id="footer">
-    <a target="_blank" href="https://github.com/RosaCL"><img src="./ressources/img/costureza.png" alt=""></a>
-</footer>
+
+
+    </section>
+    <footer class="footer" id="footer">
+        <a target="_blank" href="https://github.com/RosaCL"><img src="./ressources/img/costureza.png" alt=""></a>
+    </footer>
 </body>
+
 </html>
--- update.php
+++ update.php
@@ -8,6 +8,7 @@ include('index.php');
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,8 +16,9 @@ include('index.php');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./ressources/css/style.css">
 </head>
+
 <body>
-    <header class="header"  id="header">
+    <header class="header" id="header">
         <a href="#" class="logo"><img src="./ressources/img/logo.png" alt=""></a>
         <nav class="navbar">
             <a href="#cadastro">Cadastro</a>
@@ -25,43 +27,44 @@ include('index.php');
         <div id="menu-btn" class="fas fa-bars"></div>
     </header>
 
-<section class="cadastro" id="cadastro">
-    <div class="row">
-        <div class="box">
-            <form  method="POST">
-                
-                <input class="box" type="hidden" name="id" value="<?=$product['id']?>">
+    <section class="cadastro" id="cadastro">
+        <div class="row">
+            <div class="box">
+                <form method="POST">
 
-                <label for="nome">Nome</label>
-                <input class="box" type="text" name="nome" id="nome" placeholder="Nome"  value="<?=$product['nome']?>">
-    
-                <label for="quantidade">Quantidade</label>
-                <input class="box" type="number" name="quantidade" id="quantidade" placeholder="Quantidade"  value="<?=$product['quantidade']?>">
-    
-                <label for="unidade">Unidade</label>
-                <input class="box" type="number" name="unidade" id="unidade" placeholder="Unidade" value="<?=$product['unidade']?>">
-    
-                <label for="preco">Preço</label>
-                <input class="box" type="number" name="preco" id="preco" placeholder="Preço" step="0.010"   maxlength="10" min="0" max="9999999999"  value="<?=$product['preco']?>">
-    
-                <select class="box" name="categoria_id" required>
-                <option class="box" value="<?= $categoria['id_categoria'] ?>">Categoria</option>
-                <?php foreach ($categorias as $categoria): ?>
-                    <option value="<?= $categoria['id_categoria'] ?>"><?= $categoria['nome_categoria'] ?></option>
-                <?php endforeach; ?>
-                </select>
-    
-                        
-            <input class="btn" name="update" type="submit" value="Atualizar">
-            </form>
+                    <input class="box" type="hidden" name="id" value="<?= $product['id'] ?>">
+
+                    <label for="nome">Nome</label>
+                    <input class="box" type="text" name="nome" id="nome" placeholder="Nome" value="<?= $product['nome'] ?>">
+
+                    <label for="quantidade">Quantidade</label>
+                    <input class="box" type="number" name="quantidade" id="quantidade" placeholder="Quantidade" value="<?= $product['quantidade'] ?>">
+
+                    <label for="unidade">Unidade</label>
+                    <input class="box" type="number" name="unidade" id="unidade" placeholder="Unidade" value="<?= $product['unidade'] ?>">
+
+                    <label for="preco">Preço</label>
+                    <input class="box" type="number" name="preco" id="preco" placeholder="Preço" step="0.010" maxlength="10" min="0" max="9999999999" value="<?= $product['preco'] ?>">
+
+                    <select class="box" name="categoria_id" required>
+                        <option class="box" value="<?= $categoria['id_categoria'] ?>">Categoria</option>
+                        <?php foreach ($categorias as $categoria): ?>
+                            <option value="<?= $categoria['id_categoria'] ?>"><?= $categoria['nome_categoria'] ?></option>
+                        <?php endforeach; ?>
+                    </select>
+
+
+                    <input class="btn" name="update" type="submit" value="Atualizar">
+                </form>
+            </div>
+            <div class="image">
+                <img src="./ressources/img/banner.png" alt="">
+            </div>
         </div>
-        <div class="image">
-            <img src="./ressources/img/banner.png" alt="">
-        </div>
-    </div>
-</section>
-<footer class="footer" id="footer">
-    <a target="_blank" href="https://github.com/RosaCL"><img src="./ressources/img/costureza.png" alt=""></a>
-</footer>
+    </section>
+    <footer class="footer" id="footer">
+        <a target="_blank" href="https://github.com/RosaCL"><img src="./ressources/img/costureza.png" alt=""></a>
+    </footer>
 </body>
+
 </html>
